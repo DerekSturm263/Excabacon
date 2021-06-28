820bbebe415d2a594202b0b8a5097f0123c5828e
chickenproductions <chickenproductions@users.noreply.github.com>
2021-06-28 12:42:14 -0700

    Converted Project to 2020.3
    upgrade 😎😎😎

--- Assets/Settings/Input System/Controls.cs
+++ Assets/Settings/Input System/Controls.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Settings/Controls.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Settings/Input System/Controls.inputactions'
 
 using System;
 using System.Collections;
